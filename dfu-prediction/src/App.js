0dc98b8571b34ab23cad0e7c056b4efe7386c184
chintu 4119T <chintu4119t@gmail.com>
2025-08-23 09:45:50 +0530

    added logo

--- dfu-prediction/src/App.js
+++ dfu-prediction/src/App.js
@@ -82,9 +82,18 @@ const Sidebar = ({ activePage, setActivePage, handleSignOut }) => {
         <aside className="w-72 bg-white shadow-lg border-r border-gray-200 flex flex-col">
             <div className="p-6 border-b border-gray-200">
                 <div className="flex items-center space-x-3">
-                    <div className="w-10 h-10 bg-gradient-to-r from-blue-600 to-indigo-600 rounded-xl flex items-center justify-center">
-                        <Icon className="w-6 h-6 text-white">{ICONS.shield}</Icon>
-                    </div>
+                   <img 
+  src="/logo3.png" 
+  alt="DFU Logo" 
+  style={{ 
+    width: "50px", 
+    height: "80px", 
+    
+    padding: "10px", 
+    borderRadius: "10px" 
+  }} 
+/>
+
                     <div>
                         <h1 className="text-xl font-bold text-gray-900">DFU Analyzer</h1>
                         <p className="text-xs text-gray-500">AI-Powered Diagnostics</p>
